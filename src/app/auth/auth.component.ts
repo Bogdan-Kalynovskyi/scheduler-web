77fc4db54d68f577fcf85bd378c429f45f72ae4b
Bohdan Kalynovskyi <bohdan.kalynovkyi@contractors.roche.com>
2017-11-15 15:28:08 +0100

    bug fix, codestyle

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,50 +1,49 @@
-import { Component, OnInit } from '@angular/core'
-import {  AuthService, GoogleLoginProvider, SocialUser } from "angular4-social-login";
+import {Component, OnInit} from '@angular/core';
+import {AuthService, GoogleLoginProvider, SocialUser} from "angular4-social-login";
 
-import { UserService } from '../services/user.service';
+import {UserService} from '../services/user.service';
 
 @Component({
-	selector:'auth',
-	templateUrl:'./auth.component.html',
-	styleUrls:['./auth.component.css']
+  selector: 'auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
 })
 
-export class AuthComponent implements OnInit{
+export class AuthComponent implements OnInit {
 
-	loggedIn:boolean
+  loggedIn: boolean;
 
-	constructor(
-		private userService: UserService,
-		private authService: AuthService
-	) { }
- 
-	ngOnInit() {
-  	this.loggedIn = this.userService.isAuthenticated()
+  constructor(private userService: UserService,
+              private authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.loggedIn = this.userService.isAuthenticated();
   }
 
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID)
-    	.then((user:SocialUser) => this.userService.authenticate(user)
-    		.subscribe((user:User) => {
-    			this.userService.saveUser(user)
-    			this.loggedIn = true;
-    		}))
+    .then((user: SocialUser) => this.userService.authenticate(user)
+    .subscribe((_user: User) => {
+      this.userService.saveUser(_user);
+      this.loggedIn = true;
+    }));
   }
- 
+
   signOut(): void {
     this.authService.signOut()
-    	.then(() => {
-    		this.userService.logout()
-    		this.loggedIn = false
-    	});
+    .then(() => {
+      this.userService.logout();
+      this.loggedIn = false;
+    });
   }
 }
 
-interface User{
-	name:String,
-	id:String,
-	email:String,
-	photoUrl:String,
-	token:String,
-	expires:String
+interface User {
+  name: string;
+  id: string;
+  email: string;
+  photoUrl: string;
+  token: string;
+  expires: string;
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,39 +1,44 @@
-import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import * as moment from 'moment'
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import * as moment from 'moment';
 
-import { environment } from '../../environments/environment'
+import {environment} from '../../environments/environment';
 
 @Injectable()
 
 export class UserService {
 
-	constructor(
-		private http:HttpClient
-	){}
+  constructor(private http: HttpClient) {}
 
-	authenticate(user){
-		return this.http.post(environment.url + '/authenticate',user)
-	}
-	isAuthenticated(){
-		let tokenExp = this.getUser().expires
-		if(tokenExp > moment().valueOf){
-			console.log('expired')
-			this.logout()
-			return false
-		}else {
-				console.log('token valid')
-				return true
-			}
-	}
+  authenticate(user) {
+    return this.http.post(environment.url + '/authenticate', user);
+  }
 
-	saveUser(user){
-		localStorage.setItem('user',JSON.stringify(user))
-	}
-	getUser(){
-		return JSON.parse(localStorage.getItem('user'))
-	}
-	logout(){
-		localStorage.removeItem('user')
-	}
+  isAuthenticated() {
+    const user = this.getUser();
+    if (!user) {
+      return;
+    }
+    const tokenExp = user.expires;
+    if (tokenExp > moment().valueOf) {
+      console.log('expired');
+      this.logout();
+      return false;
+    } else {
+      console.log('token valid');
+      return true;
+    }
+  }
+
+  saveUser (user) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getUser() {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+  }
 }
